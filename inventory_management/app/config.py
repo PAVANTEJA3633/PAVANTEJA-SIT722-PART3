759e99884dbfa7e0084206acd374ccd68d8bdf22
pavanteja <pavantejaalla9999@gmail.com>
2024-09-29 21:21:25 +1000

    initial commit

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://pavanteja_sit722_part3_user:rYIO3yjWLxdhlFO9vBqX78q3JCCEdbYm@dpg-crsia80gph6c738tsb0g-a.oregon-postgres.render.com/pavanteja_sit722_part3')
--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://pavanteja_sit722_part3_user:rYIO3yjWLxdhlFO9vBqX78q3JCCEdbYm@dpg-crsia80gph6c738tsb0g-a.oregon-postgres.render.com/pavanteja_sit722_part3" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
